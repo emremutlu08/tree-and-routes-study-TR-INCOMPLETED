5594941f9b521a9493f97076874168ad514edb5c
EMRE MUTLU <mutluemre93@gmail.com>
2022-09-13 15:26:57 +0300

    README güncellendi

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 ## Görev - 1 - Router Kullanımı
 
-- Senaryo: Biz uygulamada 2 farklı sayfa oluşturmak istiyoruz. Bu sayfaların biri "Home" diğeri ise "Tree" olacak. "Home" sayfasında bir "Tree" componenti olacak. Bu "Tree" componenti "Tree" sayfasında da olacak. Bu sayfalara ait route'ları oluşturup "Home" sayfasına yönlendiren bir link oluşturunuz.
+- Senaryo: Biz uygulamada 2 farklı sayfa oluşturmak istiyoruz. Bu sayfaların biri "Home" diğeri ise "Tree Page" olacak. "Home" sayfasında bir "Tree Page" linki olacak. "Tree Page" sayfasında "Tree" Component'ı olacak. Bu sayfalara ait route'ları oluşturunuz.
+- Kullanacağımız Route Kütüphanesi: https://reactrouter.com/en/main
 
 ## Görev - 2 - Tree Yapısı Kullanımı
 
-- Senaryo: Bir servisten bize Tree Component ile uyumlu olmayan ancak Tree Component ile sergilenmesi gereken bir json objesi dönmüştür. Ve bizim bu objeyi Tree Component ile uyumlu hale getirmemiz gerekmektedir. (Not: "ItemList" içerisinde sonu belirli olmayan bir şekilde iç içe "ItemList" dizileri bulunmaktadır. public > json > tree.json içerisindeki "ItemList" sayısı artabilir / azalabilir.)
+- Senaryo: Bir servisten bize Tree Component ile uyumlu olmayan ancak Tree Component ile sergilenmesi gereken bir json objesi dönmüştür. Ve bizim bu objeyi Tree Component ile uyumlu hale getirmemiz gerekmektedir. (Not: "ItemList" içerisinde sonu belirli olmayan bir şekilde iç içe "ItemList" dizileri bulunmaktadır.
+  "public > json > tree.json" içerisindeki "ItemList" sayısı artabilir / azalabilir.)
 - Kullanacağımız Tree Component'ı => https://www.primefaces.org/primereact/tree/
 
 ### İpuçları:
@@ -16,4 +18,4 @@
 - Tree Component'ı "ItemList" yerine "children" isminde dizileri bekliyor.
 - Tree Component'ı "DisplayName" yerine "label" isminde bir değer bekliyor.
 
-- Sizden beklentimiz public > json > tree.json dosyasına dokunmadan sadece yukarıdaki "data" objesini "children" ve "label" isimlerine göre düzenleyerek (veya ekleyerek) Tree Component'ı ile uyumlu hale getirmenizdir.
+- Sizden beklentimiz "public > json > tree.json" dosyasına müdahale etmeden, sadece "data" objesini "children" ve "label" isimlerine göre düzenleyerek (veya ekleyerek) Tree Component'ı ile uyumlu hale getirmenizdir.
--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ function App() {
   /*
   * Görev - 1 - Router Kullanımı
 
-  * Senaryo: Biz uygulamada 2 farklı sayfa oluşturmak istiyoruz. Bu sayfaların biri "Home" diğeri ise "Tree" olacak. "Home" sayfasında bir "Tree" componenti olacak. Bu "Tree" componenti "Tree" sayfasında da olacak. Bu sayfalara ait route'ları oluşturup "Home" sayfasına yönlendiren bir link oluşturunuz.
+  * Senaryo: Biz uygulamada 2 farklı sayfa oluşturmak istiyoruz. Bu sayfaların biri "Home" diğeri ise "Tree Page" olacak. "Home" sayfasında bir "Tree Page" linki olacak. "Tree Page" sayfasında "Tree" Component'ı olacak. Bu sayfalara ait route'ları oluşturunuz.
+  * Kullanacağımız Route Kütüphanesi: https://reactrouter.com/en/main
   */
 
   /*
   * Görev - 2 - Tree Yapısı Kullanımı
 
-  * Senaryo: Bir servisten bize Tree Component ile uyumlu olmayan ancak Tree Component ile sergilenmesi gereken bir json objesi dönmüştür. Ve bizim bu objeyi Tree Component ile uyumlu hale getirmemiz gerekmektedir. (Not: "ItemList" içerisinde sonu belirli olmayan bir şekilde iç içe "ItemList" dizileri bulunmaktadır. public > json > tree.json içerisindeki "ItemList" sayısı artabilir / azalabilir.)
+  * Senaryo: Bir servisten bize Tree Component ile uyumlu olmayan ancak Tree Component ile sergilenmesi gereken bir json objesi dönmüştür. Ve bizim bu objeyi Tree Component ile uyumlu hale getirmemiz gerekmektedir. (Not: "ItemList" içerisinde sonu belirli olmayan bir şekilde iç içe "ItemList" dizileri bulunmaktadır.
+  * "public > json > tree.json" içerisindeki "ItemList" sayısı artabilir / azalabilir.)
   * Kullanacağımız Tree Component'ı => https://www.primefaces.org/primereact/tree/ 
 
   ? İpuçları:
@@ -27,8 +29,7 @@ function App() {
   ? "ItemList" yerine "children" isminde dizileri bekliyor.
   ? "DisplayName" yerine "label" isminde bir değer bekliyor.
   
-  ? Sizlerden beklentimiz public > json > tree.json dosyasına dokunmadan sadece yukarıdaki "data" objesini
-  ? "children" ve "label" isimlerine göre düzenleyerek (veya ekleyerek) Tree Component'ı ile uyumlu hale getirmenizdir.
+  ? Sizden beklentimiz "public > json > tree.json" dosyasına müdahale etmeden, sadece "data" objesini "children" ve "label" isimlerine göre düzenleyerek (veya ekleyerek) Tree Component'ı ile uyumlu hale getirmenizdir.
   */
 
   return (
